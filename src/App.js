035466f945c505eb418ad641de32616e087ea3e2
CryptoN999 <nicholasgallucci4@gmail.com>
2019-09-26 16:13:51 -0700

    finished MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Photos from "./components/Photos"
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <h1>NASA by Nick</h1>
+      <h3>Photo of the Day</h3>
+      <Photos/>
     </div>
   );
 }
---
+++ src/components/Photos.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import IOD from "./IOD";
+
+export default function Photos(){
+    const[photo, setPhoto] = useState([]);
+    
+    useEffect(() => {
+        axios
+            .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+            .then(resp => {
+                const photo = resp.data;
+                console.log("got em", photo);
+                setPhoto(photo);
+            })
+            .catch(error => {
+                console.log("whoops", error);
+            });
+    },[]);
+
+    return (
+        <div className="photo">
+            <IOD 
+            title= {photo.title}
+            picture= {photo.url}
+            date= {photo.date}
+            description= {photo.explanation}
+            />
+        </div>
+    )
+}
